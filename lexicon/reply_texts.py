8b7aba74e39f3810843d95ffbc8d9e301eea51b8
alexander <sadikov.af@ssmu.ru>
2025-07-22 18:11:37 +0700

    add lexicon

---
+++ lexicon/reply_texts.py
@@ -0,0 +1,16 @@
+from aiogram.types import Message, CallbackQuery
+
+
+def get_user_names(update: Message | CallbackQuery) -> str:
+    return " ".join(filter
+                    (bool,
+                     (update.from_user.first_name,
+                      update.from_user.last_name,
+                      update.from_user.username,
+                      )
+                     )
+                    )
+
+
+def create_hello_msg(update: Message | CallbackQuery) -> str:
+    return f"Привет {get_user_names(update)}"
