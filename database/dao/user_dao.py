595e09afb18d84ae93af77e748368b071ea75a92
alexander <sadikov.af@ssmu.ru>
2025-07-23 15:23:25 +0700

    change user dao

--- database/dao/user_dao.py
+++ database/dao/user_dao.py
@@ -1,8 +1,7 @@
-from database.dao.base_dao import GetOneItemDAO, GetPackItemsDAO, AddOneItemDAO
+from database.dao.base_dao import GetOneItemDAO, AddOneItemDAO
 from database.models import User
 
 
 class UserAccessUserDAO(GetOneItemDAO,
-                        GetPackItemsDAO,
                         AddOneItemDAO):
     model = User
