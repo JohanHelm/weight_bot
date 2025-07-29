96167323ef20dfba432165218da3aadcb17a6f41
alexander <sadikov.af@ssmu.ru>
2025-07-29 16:28:56 +0700

    add calculate weight gain to helpers

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,10 @@
+from math import fabs
+from typing import Any
 from aiogram import Bot
 from aiogram.types import CallbackQuery, InputMediaPhoto, BufferedInputFile, InlineKeyboardMarkup
 from aiogram.exceptions import TelegramBadRequest
+
+from config_data.initial_settings import AppParams
 
 
 async def edit_message_media(callback: CallbackQuery,
@@ -23,3 +27,14 @@ async def edit_message_media(callback: CallbackQuery,
             media=InputMediaPhoto(media=media, caption=caption),
             reply_markup=markup
         )
+
+
+def calculate_weight_gain(last_week: float, previous_week: float) -> float:
+    natural_weight_fluctuations = round(max(last_week, previous_week) * AppParams.threshold_percent / 100, 2)
+    weight_difference = round(last_week - previous_week, 2)
+
+    if fabs(weight_difference) <= natural_weight_fluctuations:
+        return 0
+    else:
+        return weight_difference
+
