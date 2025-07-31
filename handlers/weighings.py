a9889b7b0e7d400c5fecf245af0cdf4b0f28d37f
alexander <sadikov.af@ssmu.ru>
2025-07-31 12:59:24 +0700

    add weight data confirmation

--- handlers/weighings.py
+++ handlers/weighings.py
@@ -1,35 +1,27 @@
 import random
-from _datetime import datetime, UTC
-from aiogram import Router, F, Bot
-from aiogram.filters import CommandStart, StateFilter, Command
-from aiogram.types import Message, ContentType, CallbackQuery, BufferedInputFile
+from _datetime import date
+
+from aiogram import Bot, F, Router
+from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import default_state, State, StatesGroup
+from aiogram.fsm.state import default_state
+from aiogram.types import CallbackQuery, Message
 from dishka.integrations.aiogram import FromDishka
 
-from database.dao.weight_dao import UserAccessWeightsDAO
-from lexicon.reply_texts import (create_weighing_data_msg,
-                                 create_hello_msg,
-                                 bad_weighing_data_msg,
-                                 got_new_weighing_data_msg,
-                                 create_track_weight_msg,
-                                 create_not_enough_data_msg,
-                                 )
-
-from markups.keyboards import main_keyboard, back_keyboard, confirm_keyboard
+from config_data.config import TgBot
 from config_data.logging_settings import configure_logger
-from config_data.config import Config, TgBot
-from database.dao.user_dao import UserAccessUserDAO
-from utils.helpers import edit_message_media, correct_weighing_data
-from config_data.initial_settings import AppParams
-# from database.db_client import Database
-# from services.plot_service import create_plot
-# from services.sensor import get_temp_hum
-# from utils.helpers import edit_message_media, correct_temp_setting, correct_hum_setting
+from database.dao.weight_dao import UserAccessWeightsDAO
+from lexicon.reply_texts import (
+    already_entered_weighing_data,
+    bad_weighing_data_msg,
+    create_confirmed_weighing_msg,
+    create_hello_msg,
+    create_weighing_data_msg,
+    got_new_weighing_data_msg,
+)
+from markups.keyboards import back_keyboard, confirm_keyboard, main_keyboard
 from states.fsm import FSMWeighingForm
-
-
-
+from utils.helpers import correct_weighing_data, edit_message_media
 
 logger = configure_logger(__name__)
 weighings_router = Router()
@@ -53,36 +45,44 @@ async def process_cancel_command_state(callback: CallbackQuery,
 async def set_weighing_data(callback: CallbackQuery,
                             bot: Bot,
                             bot_config: FromDishka[TgBot],
+                            weight_dao: FromDishka[UserAccessWeightsDAO],
                             state: FSMContext):
-    await edit_message_media(callback,
-                             bot,
-                             random.choice(bot_config.bot_pic),
-                             back_keyboard,
-                             create_weighing_data_msg(),
-                             )
-    await state.set_state(FSMWeighingForm.enter_weighing_data)
-    await state.update_data(call=callback)
+    last_weigh_data = weight_dao.get_one({"user_id": callback.from_user.id, "date": str(date.today())})
+    if last_weigh_data is None:
+        await edit_message_media(callback,
+                                 bot,
+                                 random.choice(bot_config.bot_pic),
+                                 back_keyboard,
+                                 create_weighing_data_msg(),
+                                 )
+        await state.set_state(FSMWeighingForm.enter_weighing_data)
+        await state.update_data(call=callback)
+    else:
+        await callback.answer(text=already_entered_weighing_data,
+                              show_alert=True,
+                              cache_time=3,
+                              )
 
 
 @weighings_router.message(StateFilter(FSMWeighingForm.enter_weighing_data), )
 async def process_weighing_data(msg: Message,
-                               bot: Bot,
-                               bot_config: FromDishka[TgBot],
-                               state: FSMContext,
-                               ):
+                                bot: Bot,
+                                bot_config: FromDishka[TgBot],
+                                state: FSMContext,
+                                ):
     if correct_weighing_data(msg.text):
         await msg.delete()
         state_data = await state.get_data()
         callback = state_data.get("call")
         weigh_data = round(float(msg.text), 2)
-        # db_instance.set_alarm_setting("temp", new_temp_setting)
-        await state.clear()
+        await state.update_data(weigh_data=weigh_data)
         await edit_message_media(callback,
                                  bot,
                                  random.choice(bot_config.bot_pic),
                                  confirm_keyboard,
                                  got_new_weighing_data_msg(weigh_data),
                                  )
+        await state.set_state(FSMWeighingForm.confirm_weighing_data)
     else:
         await msg.delete()
         state_data = await state.get_data()
@@ -93,3 +93,27 @@ async def process_weighing_data(msg: Message,
                                  back_keyboard,
                                  f"{bad_weighing_data_msg}{create_weighing_data_msg()}",
                                  )
+
+
+@weighings_router.callback_query(F.data == "confirm_btn", StateFilter(FSMWeighingForm.confirm_weighing_data), )
+async def confirm_weighing_data(callback: CallbackQuery,
+                                bot: Bot,
+                                bot_config: FromDishka[TgBot],
+                                weight_dao: FromDishka[UserAccessWeightsDAO],
+                                state: FSMContext,
+                                ):
+    state_data = await state.get_data()
+    weigh_data = state_data.get("weigh_data")
+    weight_dao.add_one({"user_id": callback.from_user.id, "weight": weigh_data, "date": date.today()})
+
+    await callback.answer(text=create_confirmed_weighing_msg(weigh_data),
+                          show_alert=True,
+                          cache_time=3,
+                          )
+    await edit_message_media(callback,
+                             bot,
+                             random.choice(bot_config.bot_pic),
+                             main_keyboard,
+                             create_hello_msg(callback),
+                             )
+    await state.clear()
