686c2b382284cc16544b2ff637f4eb1a10565076
alexander <sadikov.af@ssmu.ru>
2025-08-01 16:26:43 +0700

    add pagination keyboard and buttons

--- markups/buttons.py
+++ markups/buttons.py
@@ -8,4 +8,11 @@ track_btn = InlineKeyboardButton(text="👣 ТРЕКЕР ВЕСА", callback_dat
 
 confirm_btn = InlineKeyboardButton(text="👇 ПОДТВЕРДИТЬ", callback_data="confirm_btn")
 
-plot_btn =  InlineKeyboardButton(text="📈 ГРАФИК", callback_data="plot_btn")
+plot_btn =  InlineKeyboardButton(text="📈 ГРАФИК", callback_data="0")
+
+page_forward_btn = InlineKeyboardButton(text="➡️️", callback_data="-1")
+
+page_backward_btn = InlineKeyboardButton(text="⬅️", callback_data="1")
+
+def create_pages_total_btn(page:int, total_pages: int) -> InlineKeyboardButton:
+    return InlineKeyboardButton(text=f"{page} / {total_pages}️", callback_data="total_pages")
--- markups/keyboards.py
+++ markups/keyboards.py
@@ -19,3 +19,17 @@ confirm_keyboard = InlineKeyboardMarkup(
                      [backward_btn],
                      ]
 )
+
+def page_keyboard(page:int, total_pages: int)->InlineKeyboardMarkup:
+    if page == total_pages:
+        first_row = [create_pages_total_btn(page, total_pages), page_forward_btn]
+    elif page == 0:
+        first_row = [page_backward_btn, create_pages_total_btn(page, total_pages)]
+    else:
+        first_row = [page_backward_btn, create_pages_total_btn(page, total_pages), page_forward_btn]
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=[first_row,
+                         [backward_btn],
+                         ]
+    )
