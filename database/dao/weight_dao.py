72bd88a726bd71de48f0d700d5cccc10fa0abdd3
alexander <sadikov.af@ssmu.ru>
2025-08-01 12:50:59 +0700

    add some imports

--- database/dao/weight_dao.py
+++ database/dao/weight_dao.py
@@ -1,9 +1,11 @@
-from database.dao.base_dao import AddOneItemDAO, GetAwgDataDAO, GetCountItemsDAO
+from database.dao.base_dao import AddOneItemDAO, GetAwgDataDAO, GetCountItemsDAO, GetOneItemDAO, GetPackItemsDAO
 from database.models import Weights
 
 
 class UserAccessWeightsDAO(GetAwgDataDAO,
                            GetCountItemsDAO,
                            AddOneItemDAO,
+                           GetOneItemDAO,
+                           GetPackItemsDAO,
                            ):
     model = Weights
