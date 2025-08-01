c05ac94c8e5d440feee8eeaa82556a73265e6e5c
alexander <sadikov.af@ssmu.ru>
2025-08-01 16:28:07 +0700

    add page state

--- states/fsm.py
+++ states/fsm.py
@@ -4,3 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class FSMWeighingForm(StatesGroup):
     enter_weighing_data = State()
     confirm_weighing_data = State()
+
+
+class FSMPageForm(StatesGroup):
+    set_page = State()
