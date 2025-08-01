3eacaa169057206dd7a97e7ca284c4d24899a2b5
alexander <sadikov.af@ssmu.ru>
2025-08-01 12:49:16 +0700

    add PlotParams

--- config_data/initial_settings.py
+++ config_data/initial_settings.py
@@ -24,12 +24,16 @@ class AppParams:
     threshold_percent: float = 1.5
 
 
-# @dataclass(frozen=True)
-# class PlotParams:
-#     plot_title: str = "Результаты измерений за "
-#     label_temp: str = "Температура, °C"
-#     label_hum: str = "Влажность, %"
-#     plot_format: str = "png"
-#     records_limit: int = 30
-#     every_on_x_axis: int = 5
-#     img_filename: str = "image from buffer.png"
+@dataclass(frozen=True)
+class PlotParams:
+    y_label: str = "Вес, кг"
+    x_label: str = "Дата"
+    plot_format: str = "png"
+    img_filename: str = "image from buffer.png"
+    width: int = 10
+    height: int = 7
+    plot_label: str = "Вес"
+    x_label_rotation: int = 90
+    marker: str = 'o'
+    trend_label="Тренд"
+    trend_color = "red"
